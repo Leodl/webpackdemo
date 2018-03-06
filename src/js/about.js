48f42ef94480790fff625e2954dd29db48297280
Leodl <1215918846@qq.com>
2018-03-06 21:16:15 +0800

    dl

--- src/js/about.js
+++ src/js/about.js
@@ -2,18 +2,31 @@
 import "@less/global"
 import "@less/about"
 import $ from "jquery"
+import common from '@js/list'
+
 
 $('body').append('这是js动态生成的内容：这是about页面！')
 
 
 
-$("#btn").click(function(){
-	alert(222)
-})
 
-function add(){
-  alert(1)
+
+var add = ()=>{
+	common.add();
+	$("#btn").click(function(){
+
+		alert(222)
+
+	})
+	$("#btn1").click(function(){
+		common.add();
+	})
 }
+add()
+
+
+
+
 
 // var about = function(){
 //     var obj = {};
--- src/js/list.js
+++ src/js/list.js
@@ -4,3 +4,16 @@ import "@less/list"
 import $ from "jquery"
 
 $('body').append('这是js动态生成的内容，这是list页面！')
+
+var common = function(){
+	var obj = {};
+	obj.add = function(){
+		alert("Leo")
+	}
+	obj.count = function(){
+		alert(3333)
+	}
+
+	return obj;
+}()
+export default common
